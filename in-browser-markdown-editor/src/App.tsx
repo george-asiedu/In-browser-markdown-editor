fee58bf394b72355a44ba6ed5e87b1d8c3271e3f
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-14 15:54:02 +0100

    global styles imported and heading global styles added

--- in-browser-markdown-editor/src/App.tsx
+++ in-browser-markdown-editor/src/App.tsx
@@ -1,10 +1,69 @@
 import './App.css'
+import styled, { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Roboto Slab', serif;
+    font-family: 'Roboto Mono', monospace;
+    font-family: 'Roboto', sans-serif;
+    overflow-x: hidden;
+  }
+
+  h1 {
+    font-family: 'Roboto Slab', serif;
+    font-weight: 700;
+    font-size: 32px;
+    color: 
+  }
+
+  h2 {
+    font-family: 'Roboto Slab', serif;
+    font-weight: 300;
+    font-size: 28px;
+    color: 
+  }
+
+  h3 {
+    font-family: 'Roboto Slab', serif;
+    font-weight: 700;
+    font-size: 24px;
+    color: 
+  }
+
+  h4 {
+    font-family: 'Roboto Slab', serif;
+    font-weight: 700;
+    font-size: 20px;
+    color: 
+  }
+
+  h5 {
+    font-family: 'Roboto Slab', serif;
+    font-weight: 700;
+    font-size: 16px;
+    color: 
+  }
+
+  h6 {
+    font-family: 'Roboto Slab', serif;
+    font-weight: 700;
+    font-size: 14px;
+    color: 
+  }
+`
 
 function App() {
 
   return (
-    <div className='app'>
-    </div>
+    <>
+    <GlobalStyle />
+    </>
   )
 }
 
