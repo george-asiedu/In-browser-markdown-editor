f36eccca012994b09b2af7c10d145a82a6f09fe2
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-15 11:18:07 +0100

    CreateButtonStyle component added

--- in-browser-markdown-editor/src/components/navigation/CreateButton.tsx
+++ in-browser-markdown-editor/src/components/navigation/CreateButton.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const CreateButtonStyle = styled.div`
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+    align-items: center;
+    width: 202px;
+    height: 40px;
+    padding: 0px 17px;
+    margin: 0px;
+    margin-right: 16px;
+    border: none;
+    border-radius: 4px;
+    color: #ffffff;
+    background-color: #e46643;
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 15px;
+    line-height: 18px;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #f39765;
+    }
+`
 
 const CreateButton: React.FC = () => {
   return (
-    <div>
-
-    </div>
+    <CreateButtonStyle>
+        + New Document
+    </CreateButtonStyle>
   )
 }
 
