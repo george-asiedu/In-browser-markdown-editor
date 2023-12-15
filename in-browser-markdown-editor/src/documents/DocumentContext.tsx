2d6a0e0fc7f790e8876ba418eac15d7cf58c7135
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-15 11:44:44 +0100

    uuid dependency imported
    the purpose of importing uuidv4 is to have a convenient and reliable way to generate unique identifiers for your documents.

---
+++ in-browser-markdown-editor/src/documents/DocumentContext.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import textDocuments from './data.json'
+import { v4 as uuidv4 } from "uuid";
+
+const DocumentContext: React.FC = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default DocumentContext
