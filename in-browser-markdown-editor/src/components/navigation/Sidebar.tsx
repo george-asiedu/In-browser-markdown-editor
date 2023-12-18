5db21cd7e7257f207efa71aa274e0ecd8a615933
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-18 10:15:58 +0100

    SidebarStyles styled component defined

--- in-browser-markdown-editor/src/components/navigation/Sidebar.tsx
+++ in-browser-markdown-editor/src/components/navigation/Sidebar.tsx
@@ -2,6 +2,23 @@ import React from 'react'
 import styled from 'styled-components'
 import iconDocument from '../../assets/icon-document.svg'
 
+const sidebarStyles = styled.div`
+    height: 100vh;
+    width: 250px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: #1d1f22;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: space-between;
+
+    transform: translateX(
+    ${({ showSidebar }) => (showSidebar ? "0px" : "-250px")}
+    );
+    transition: 0.3s;
+`
+
 const Sidebar: React.FC = () => {
     return (
         
