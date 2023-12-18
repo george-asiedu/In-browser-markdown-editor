d15ac0f7889cb1c550753e2c7903215be67654a6
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-18 08:35:42 +0100

    DocumentContainer styled component

--- in-browser-markdown-editor/src/components/navigation/Navbar.tsx
+++ in-browser-markdown-editor/src/components/navigation/Navbar.tsx
@@ -76,6 +76,22 @@ const Divider = styled.div`
     }
 `
 
+const DocumentContainer = styled.div`
+  height: 36px;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-self: center;
+
+  @media screen and (max-width: 1020px) {
+    margin-left: 24px;
+  }
+
+  @media screen and (max-width: 768px) {
+    height: 18px;
+  }
+`
+
 const Navbar: React.FC = () => {
   return (
     <div>
