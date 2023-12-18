637dba778162bb244ec915963acbb2f0854ce3f4
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-18 09:58:52 +0100

    Navbar component imported
    havbar props set to Navigation props

--- in-browser-markdown-editor/src/components/navigation/Navigation.tsx
+++ in-browser-markdown-editor/src/components/navigation/Navigation.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
+import Navbar from './Navbar'
 
 const NavigationStyles = styled.div``
 
-const Navigation: React.FC = () => {
-  return (
-    <div>Navigation</div>
-  )
+const Navigation: React.FC = ({
+    showSidebar,
+    handleSidebar,
+    handleEnter,
+    setModalOpen
+}) => {
+
+    return (
+        <NavigationStyles>
+            <Navbar
+                showSidebar={showSidebar}
+                handleSidebar={handleSidebar}
+                handleEnter={handleEnter}
+                setModalOpen={setModalOpen} 
+            />
+        </NavigationStyles>
+    )
 }
 
 export default Navigation
