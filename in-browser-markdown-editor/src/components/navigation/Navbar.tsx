8d52dd4220928328fb973909886b7e233527ece7
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-18 08:22:23 +0100

    NavbarStyles styled component added

--- in-browser-markdown-editor/src/components/navigation/Navbar.tsx
+++ in-browser-markdown-editor/src/components/navigation/Navbar.tsx
@@ -2,7 +2,23 @@ import React from 'react'
 import styled from 'styled-components'
 
 
+const NavbarStyles = styled.div`
+    width: 100vw;
+    height: 72px;
+    color: #ffffff;
+    background-color: #2b2d31;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    transform: translateX(
+    ${({ showSidebar }) => (showSidebar ? "250px" : "0px")}
+    );
+    transition: 0.3s;
 
+    @media screen and (max-width: 768px) {
+    height: 56px;
+    }
+`
 
 const Navbar: React.FC = () => {
   return (
