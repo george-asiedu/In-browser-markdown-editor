2c752c960c60df0ca728486f9515b694c6cf5bef
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-18 12:59:17 +0100

    Update on App component

--- in-browser-markdown-editor/src/App.tsx
+++ in-browser-markdown-editor/src/App.tsx
@@ -1,21 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useRef } from 'react';
 import styled, { ThemeProvider } from 'styled-components'
 import { themes } from './themes/Themes';
 import { ThemeContext, ThemeContextProvider } from './themes/ThemeContext';
 import { GlobalStyle } from './GlobalStyles.module';
-
-
+import Navigation from './components/navigation/Navigation';
 
 const StyledApp = styled.div``;
 
 const App: React.FC = () => {
   const { theme } = useContext(ThemeContext)
+  const [showSidebar, setShowSidebar] = useState(false)
+  const [modalOpen, setModalOpen] = useState(false)
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSidebar = () => {
+    setShowSidebar(showSidebar ? false : true)
+  }
+
+  const handleEnter = () => {
+    inputRef.current?.focus()
+  }
 
   return (
     <ThemeContextProvider>
       <ThemeProvider theme={theme}>
         <GlobalStyle theme={themes.light}/>
-        <StyledApp></StyledApp>
+        <StyledApp>
+            <Navigation
+                showSidebar={showSidebar}
+                handleSidebar={handleSidebar}
+                handleEnter={handleEnter}
+                setModalOpen={setModalOpen}
+             />
+        </StyledApp>
       </ThemeProvider>     
     </ThemeContextProvider>
   )
