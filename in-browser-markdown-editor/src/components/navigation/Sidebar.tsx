857d16db85e633ef788248b6a5a01c4295b9afa5
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-18 10:14:21 +0100

    Sidebar component created and style component imported

---
+++ in-browser-markdown-editor/src/components/navigation/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+import iconDocument from '../../assets/icon-document.svg'
+
+const Sidebar: React.FC = () => {
+    return (
+        
+    )
+}
+
+export default Sidebar
