68948756745c6ba05d63d3c3286d2dbaaf123b7c
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-18 09:32:58 +0100

    SaveButtonStyles styled component added

--- in-browser-markdown-editor/src/components/navigation/SaveButton.tsx
+++ in-browser-markdown-editor/src/components/navigation/SaveButton.tsx
@@ -1,6 +1,42 @@
 import React from 'react'
+import styled from 'styled-components'
+import iconSave from '../../assets/icon-save.svg'
 
-const SaveButton = () => {
+
+const SaveButtonStyles = styled.button`
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    align-items: center;
+    width: 152px;
+    height: 40px;
+    padding: 0px 17px;
+    margin: 0px;
+    margin-right: 16px;
+    border: none;
+    border-radius: 4px;
+    color: #ffffff;
+    background-color: #e46643;
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 15px;
+    line-height: 18px;
+    cursor: pointer;
+
+    &:hover {
+        background-color: ${({ disabled }) => (disabled ? "#e46643" : "#f39765")};
+    }
+
+    @media screen and (max-width: 768px) {
+        width: 40px;
+        justify-content: center;
+        padding: 0;
+        margin-right: 8px;
+    }
+`
+
+const SaveButton: React.FC = () => {
   return (
     <div>SaveButton</div>
   )
