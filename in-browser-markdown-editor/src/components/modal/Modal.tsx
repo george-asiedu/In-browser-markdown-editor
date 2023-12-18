17161163ed72896d5f00c89c5c921cbaea5d00e8
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-18 14:17:51 +0100

    ModalStyles styled component created

--- in-browser-markdown-editor/src/components/modal/Modal.tsx
+++ in-browser-markdown-editor/src/components/modal/Modal.tsx
@@ -2,6 +2,20 @@ import React from 'react'
 import styled from 'styled-components'
 import Modal from 'react-modal'
 
+const ModalStyles = styled(Modal)`
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    right: auto;
+    bottom: auto;
+    margin-right: -50%;
+    transform: translate(-50%, -50%);
+    padding: 24px;
+    display: flex;
+    flex-flow: column nowrap;
+    background-color: ${({ theme }) => theme.background.main};
+`
+
 const CustomModal: React.FC = () => {
   return (
     <div></div>
