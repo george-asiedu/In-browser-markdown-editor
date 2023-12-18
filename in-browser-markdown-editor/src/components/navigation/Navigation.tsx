ac9803f86cdc02d64997daf99e54864cf2dcc60b
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-18 09:54:23 +0100

    Navigation component created
    Styled component imported and NavigationStyles style component defined

---
+++ in-browser-markdown-editor/src/components/navigation/Navigation.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const NavigationStyles = styled.div``
+
+const Navigation: React.FC = () => {
+  return (
+    <div>Navigation</div>
+  )
+}
+
+export default Navigation
