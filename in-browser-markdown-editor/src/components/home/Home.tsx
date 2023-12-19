59685155106b3f21d8f2b7a1d819398e8dc5a286
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-19 15:24:40 +0100

    HomeStyles style component defined

--- in-browser-markdown-editor/src/components/home/Home.tsx
+++ in-browser-markdown-editor/src/components/home/Home.tsx
@@ -1,10 +1,23 @@
 import React from "react"
 import styled from "styled-components"
+import { Theme } from '../../themes/Themes'
 
 interface HomeProps {
   inputRef: React.RefObject<any>
   showSidebar: boolean
 }
+
+const HomeStyles = styled.div<{ showSidebar: boolean ; theme: Theme}>`
+    display: flex;
+    flex-flow: row nowrap;
+    height: 100%;
+    width: 100%;
+    position: fixed;
+    color: ${(props) => props.theme.color.markdownbody};
+    background-color: ${(props) => props.theme.background.main};
+    transform: translateX(${({ showSidebar }) => (showSidebar ? "250px" : "0px")});
+    transition: 0.3s;
+`
 
 const Home: React.FC = () => {
   return (
