3d3180b91ba2b14b89bce2c612ce1809f27a13a1
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-19 11:05:10 +0100

    Update on MarkdownEditorWindow component

--- in-browser-markdown-editor/src/components/home/MarkdownEditorWindow.tsx
+++ in-browser-markdown-editor/src/components/home/MarkdownEditorWindow.tsx
@@ -1,75 +1,95 @@
-import React, { ChangeEvent, forwardRef, useContext } from 'react'
-import styled from 'styled-components'
-import { ThemeContext } from '../../themes/ThemeContext'
-import { DocumentContext } from '../../documents/DocumentContext'
+import React, { forwardRef, useContext, ChangeEvent, Ref } from "react";
+import styled from "styled-components";
+import { ThemeContext } from "../../themes/ThemeContext";
+import { DocumentContext } from "../../documents/DocumentContext";
 
 interface MarkdownEditorProps {
-    showPreview: boolean
-    handlePrview: () => void
+  showPreview: boolean;
+  handlePreview: () => void;
 }
 
-const MarkdownEditorStyles = styled.div<{ showPreview: boolean}>`
-    display: ${({ showPreview }) => (showPreview ? 'none': 'flex')};
-    flex-flow: column nowrap;
-    height: 100%;
-    width: 50%;
-    outline: none;
-    border: none;
+const StyledEditorContainer = styled.div<{ showPreview: boolean }>`
+  display: ${({ showPreview }) => (showPreview ? "none" : "flex")};
+  flex-flow: column nowrap;
+  height: 100%;
+  width: 50%;
+  outline: none;
+  border: none;
 
-    @media screen and (max-width: 768px) {
-        width: ${({ showPreview }) => (showPreview ? "0%" : "1000%")};
-    }
-`
+  @media screen and (max-width: 768px) {
+    width: ${({ showPreview }) => (showPreview ? "0%" : "1000%")};
+  }
+`;
 
 const TitleContainer = styled.div`
-    height: 42px;
-    font-family: "Roboto";
-    font-style: normal;
-    font-weight: 500;
-    font-size: 14px;
-    line-height: 16px;
-    letter-spacing: 2px;
-    padding-left: 16px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    color: ${({ theme }) => theme.color.sectionheader};
-    background-color: ${({ theme }) => theme.background.sectionheader};
-`
+  height: 42px;
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 16px;
+  letter-spacing: 2px;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: ${({ theme }) => theme.color.sectionheader};
+  background-color: ${({ theme }) => theme.background.sectionheader};
+`;
 
 const ShowPreviewButton = styled(PreviewButton)`
-    display: none;
-    @media screen and (max-width: 768px) {
-        display: block;
-    }
-`
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: block;
+  }
+`;
 
 const Editor = styled.textarea<{ theme: any }>`
-    height: calc(100% - 120px);
-    padding-left: 16px;
-    outline: none;
-    border: none;
-    font-family: "Roboto Mono";
-    font-style: normal;
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 24px;
-    color: ${({ theme }) => theme.color.markdownbody};
-    background-color: ${({ theme }) => theme.background.main};
-`
+  height: calc(100% - 120px);
+  padding-left: 16px;
+  outline: none;
+  border: none;
+  font-family: "Roboto Mono";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 24px;
+  color: ${({ theme }) => theme.color.markdownbody};
+  background-color: ${({ theme }) => theme.background.main};
+`;
 
-const MarkdownEditorWindow: React.FC = forwardRef<HTMLTextAreaElement, MarkdownEditorProps>(props, inputRef) => {
-    const { theme } = useContext(ThemeContext)
-    const { activeDocument, onDocumentContentChange } = useContext(DocumentContext)
+const MarkdownEditorWindow: React.FC<MarkdownEditorProps> = forwardRef(
+  (props, inputRef: Ref<HTMLTextAreaElement>) => {
+    const { theme } = useContext(ThemeContext);
+    const { activeDocument, onDocumentContentChange } = useContext(
+      DocumentContext
+    );
 
-    const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-        onDocumentContentChange(e.target.value)
-    }
-  return (
-    <div>
+    const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        onDocumentContentChange(event);
+    };
 
-    </div>
-  )
-}
+    return (
+      <StyledEditorContainer showPreview={props.showPreview}>
+        <TitleContainer theme={theme}>
+          MARKDOWN
+          <ShowPreviewButton
+            showPreview={props.showPreview}
+            handlePreview={props.handlePreview}
+            isPreviewWindow={false}
+          />
+        </TitleContainer>
+        <Editor
+          value={activeDocument.content}
+          onChange={handleChange}
+          ref={inputRef}
+          theme={theme}
+          {...props}
+        />
+      </StyledEditorContainer>
+    );
+  }
+);
 
-export default MarkdownEditorWindow
+MarkdownEditorWindow.displayName = "MarkdownEditor";
+export default MarkdownEditorWindow;
