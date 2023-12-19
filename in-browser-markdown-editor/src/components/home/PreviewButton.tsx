2f532bb06b6b4aba0013301d20949ed46d2ecf81
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-19 13:00:41 +0100

    PreviewButtonStylesProps interface defined

--- in-browser-markdown-editor/src/components/home/PreviewButton.tsx
+++ in-browser-markdown-editor/src/components/home/PreviewButton.tsx
@@ -3,6 +3,14 @@ import styled from 'styled-components'
 import iconShowPreview from '../../assets/icon-show-preview.svg'
 import iconHidePreview from '../../assets/icon-hide-preview.svg'
 
+interface PreviewButtonStylesProps {
+    theme: {
+        background: {
+            sectionheader: string
+        }
+    }
+}
+
 const PreviewButton: React.FC = () => {
   return (
     <div>
