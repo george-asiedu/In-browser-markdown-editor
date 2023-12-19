51f02cba886465fdf54f8f35a8593952c6a81821
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-19 12:59:29 +0100

    preview icons imported

--- in-browser-markdown-editor/src/components/home/PreviewButton.tsx
+++ in-browser-markdown-editor/src/components/home/PreviewButton.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import styled from 'styled-components'
+import iconShowPreview from '../../assets/icon-show-preview.svg'
+import iconHidePreview from '../../assets/icon-hide-preview.svg'
 
 const PreviewButton: React.FC = () => {
   return (
