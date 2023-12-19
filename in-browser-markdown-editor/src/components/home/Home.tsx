42304cf9a591165f125bd5d1f1fe220d94b89db9
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-19 15:17:29 +0100

    HomeProps interface defined

--- in-browser-markdown-editor/src/components/home/Home.tsx
+++ in-browser-markdown-editor/src/components/home/Home.tsx
@@ -1,6 +1,11 @@
 import React from "react"
 import styled from "styled-components"
 
+interface HomeProps {
+  inputRef: React.RefObject<any>
+  showSidebar: boolean
+}
+
 const Home: React.FC = () => {
   return (
     <div>Home</div>
