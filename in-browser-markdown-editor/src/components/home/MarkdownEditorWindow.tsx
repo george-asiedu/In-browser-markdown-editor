78626923f05c0059d2620c59bb437972ec62af7d
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-19 10:33:47 +0100

    MarkdownEditorProps interface define

---
+++ in-browser-markdown-editor/src/components/home/MarkdownEditorWindow.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface MarkdownEditorProps {
+    showPreview: boolean
+    handlePrview: () => void
+}
+
+const MarkdownEditorWindow: React.FC = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default MarkdownEditorWindow
