b1906db430d41faec3d9f263fd63ad809710032e
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-20 12:44:42 +0100

    NotificationMessage component done

--- in-browser-markdown-editor/src/notification/NotificationMessage.tsx
+++ in-browser-markdown-editor/src/notification/NotificationMessage.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import styled from 'styled-components';
+import { Theme } from '../themes/Themes';
 
 interface NotificationProps {
     message: string;
-  }
+}
 
-const NotificationMessage: React.FC = () => {
+const NotificationMessageStyles = styled.div<{ theme: Theme}>`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    padding: 10px;
+    text-align: center;
+    background-color: ${({ theme}) => theme.background.main};
+    color: ${({ theme }) => theme.color.h6}
+`
+
+const NotificationMessage: React.FC<NotificationProps> = ({ message}) => {
   return (
-    <div>NotificationMessage</div>
+    <NotificationMessageStyles>
+        {message}
+    </NotificationMessageStyles>
   )
 }
 
